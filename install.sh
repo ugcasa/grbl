b6a352a0629a77170a1069aee658dd77ac279898
Juha Palm <casa@ujo.guru>
2019-08-08 16:29:15 +0300

    installer cleanup

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Installer for giocon client. ujo.guru / juha.palm 
 
-export GURU_CALL="guru"
-export GURU_USER="$USER"
-export GURU_BIN=$HOME/bin
-export GURU_CFG=$HOME/.config/guru
+GURU_CALL="guru"
+GURU_USER="$USER"
+GURU_BIN=$HOME/bin
+GURU_CFG=$HOME/.config/guru
 
 check_python_module () {
 	python -c "import $1"; 	
@@ -13,6 +13,7 @@ check_python_module () {
 
 bashrc="$HOME/.bashrc"
 disable="$HOME/.gururc.disabled"
+
 
 ## already installed? reinstall?
 
@@ -25,40 +26,48 @@ if grep -q ".gururc" "$bashrc"; then
 	[ -f $GURU_BIN/uninstall.sh ] && bash $GURU_BIN/uninstall.sh || echo "uninstaller not found"
 fi
 
+
 # Default is server
+
  if [ $1 ]; then 
  	platform=$1
  else
- 	lsb_release -crid | grep Mint >/dev/null && platform="desktop" || echo platform="server"
+ 	lsb_release -crid | grep "Mint" >/dev/null && platform="desktop" || platform="server"
  fi
 
 
 ### .bashrc
+
 [ -f "$HOME/.bashrc.giobackup" ] || cp -f "$bashrc" "$HOME/.bashrc.giobackup"
 grep -q ".gururc" "$bashrc" || cat ./src/tobashrc.sh >>"$bashrc"
 
+
 ## folder structure copy files
+
 [ -d $GURU_CFG ] || mkdir -p $GURU_CFG
 [ -d $GURU_BIN ] || mkdir -p $GURU_BIN
 
 [ -f $disable ] && rm -f $disable 			# remove gio.disabler file
+
 cp -f ./src/gururc.sh "$HOME/.gururc"
 . $HOME/.gururc
+
 cp -f ./src/guru.sh "$GURU_BIN/$GURU_CALL"
 cp -f ./cfg/* "$GURU_CFG"
 cp -f ./src/* -f "$GURU_BIN"
 cp -f ./src/datestamp.py "$GURU_BIN/gio.datestamp"
 
-## Common debian requirements
+
+## Common requirements
 
 git --version >/dev/null || sudo apt install git
-#[ -f /usr/bin/mosquitto_pub ] || sudo apt install mosquitto-clients
+#[ -f /usr/bin/mosquitto_pub ] || sudo apt install mosquitto-clients # >> installer.sh
 pip3 help >/dev/null || sudo apt install python3-pip
 pv -V >/dev/null || sudo apt install pv 
-check_python_module feedparser >/dev/null || pip3 install feedparser	
-check_python_module virtualenv >/dev/null || pip3 install virtualenv
 
-
+# TODO -> virtual env
+#check_python_module feedparser >/dev/null || sudo pip -H install feedparser		
+#check_python_module virtualenv >/dev/null || sudo pip -H install virtualenv
 
 case $platform in 
 	
@@ -70,10 +79,13 @@ case $platform in
 		xterm -v >/dev/null || sudo apt install xterm		
 		dconf help >/dev/null || sudo apt install dconf-cli
 		new=./cfg/kbbind.guruio.cfg				
+		
 		if [ ! -f $current ]; then 		
 			dconf dump /org/cinnamon/desktop/keybindings/ > $current && cat $current | grep binding=
 		fi
+		
 		dconf load /org/cinnamon/desktop/keybindings/ < $new		
+		
 		# set up		
 		bash $GURU_CALL set audio true
 		mpsyt --version >>/dev/null || $GURU_CALL install mpsyt		
@@ -81,10 +93,11 @@ case $platform in
 
 	server)	# Server/ubuntu server no gui		
 		joe --help >/dev/null || sudo apt install joe
+		
 		# set up
+		bash $GURU_CALL set install server
 		bash $GURU_CALL set audio false
 		bash $GURU_CALL set editor joe
-		bash $GURU_CALL set install server
 		;;
 
 
