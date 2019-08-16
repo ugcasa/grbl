ce0b8a7c3b34337afd37fe624ca14106dc86aff1
Juha Palm <casa@ujo.guru>
2019-08-16 19:07:07 +0300

    guru.sdh location

--- install.sh
+++ install.sh
@@ -6,18 +6,19 @@ GURU_USER="$USER"
 GURU_BIN=$HOME/bin
 GURU_CFG=$HOME/.config/guru
 
-check_python_module () {
+
+check_python_module () {			# Does work, but returns funny
 	python -c "import $1"; 	
 	return $?
 }
 
-bashrc="$HOME/.bashrc"
-disable="$HOME/.gururc.disabled"
 
+target_rc="$HOME/.bashrc"
+disabler_flag_file="$HOME/.gururc.disabled"
 
 ## already installed? reinstall?
 
-if grep -q ".gururc" "$bashrc"; then	
+if grep -q ".gururc" "$target_rc"; then	
 	read -p "already installed, force re-install [y/n] : " answer	
 	if ! [[ "$answer" == "y" ]]; then
 		echo "aborting.."
@@ -25,7 +26,6 @@ if grep -q ".gururc" "$bashrc"; then
 	fi	
 	[ -f $GURU_BIN/uninstall.sh ] && bash $GURU_BIN/uninstall.sh || echo "uninstaller not found"
 fi
-
 
 # Default is server
 
@@ -35,34 +35,31 @@ fi
  	lsb_release -crid | grep "Mint" >/dev/null && platform="desktop" || platform="server"
  fi
 
-
 ### .bashrc
 
-[ -f "$HOME/.bashrc.giobackup" ] || cp -f "$bashrc" "$HOME/.bashrc.giobackup"
-grep -q ".gururc" "$bashrc" || cat ./src/tobashrc.sh >>"$bashrc"
+[ -f "$HOME/.bashrc.giobackup" ] || cp -f "$target_rc" "$HOME/.bashrc.giobackup"
+grep -q ".gururc" "$target_rc" || cat ./src/tobashrc.sh >>"$target_rc"
 
+[ -f $disabler_flag_file ] && rm -f $disabler_flag_file 	
 
 ## folder structure copy files
 
-[ -d $GURU_CFG ] || mkdir -p $GURU_CFG
-[ -d $GURU_BIN ] || mkdir -p $GURU_BIN
-
-[ -f $disable ] && rm -f $disable 			# remove gio.disabler file
-
 cp -f ./src/gururc.sh "$HOME/.gururc"
-. $HOME/.gururc
+. $HOME/.gururc													# rise default environmental variables
 
-cp -f ./src/guru.sh "$GURU_BIN/$GURU_CALL"
-cp -f ./cfg/* "$GURU_CFG"
-cp -f ./src/* -f "$GURU_BIN"
-cp -f ./src/datestamp.py "$GURU_BIN/gio.datestamp"
+[ -d $GURU_BIN ] || mkdir -p $GURU_BIN
+[ -d $GURU_CFG ] || mkdir -p $GURU_CFG
+cp -f ./src/guru.sh "$GURU_BIN/guru"							# to able to call just "guru"
+cp -f ./cfg/* "$GURU_CFG"										# guru toolkit configurations
+cp -f ./src/* -f "$GURU_BIN"									# guru toolkit scripts
+cp -f ./src/datestamp.py "$GURU_BIN/gio.datestamp"  			# compatibility bubblegum
 
+## Basic requirements, even dough user cannot get this far whit out these 
 
-## Common requirements
 git --version >/dev/null || sudo apt install git
 pip3 help >/dev/null || sudo apt install python3-pip
 
-# TODO -> virtual env
+# TODO -> virtual env to get rid of these 
 #check_python_module feedparser >/dev/null || sudo pip -H install feedparser		
 #check_python_module virtualenv >/dev/null || sudo pip -H install virtualenv
 
@@ -104,7 +101,10 @@ case $platform in
 		exit 4
 esac
 
-bash $GURU_CALL counter add giocon_install >/dev/null
+															# Issue: 
+bash $GURU_CALL counter add giocon_install >/dev/null		# some conflicts recorded, not fully working method to sync by Dropbox
+															# giocon_install (estella's conflicted copy 2019-08-10) "10" 
+															# giocon_install (estella's conflicted copy 2019-08-14) "67"
 echo "successfully installed"
 
 
