ad53027c50ac328a21ede76c7e3cc299b704174d
casa <casa@ujo.guru>
2019-10-25 01:26:03 +0300

    mp3 tagger added

--- src/tag.sh
+++ src/tag.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-# mick tagger - ujo.guru 2019
+# Mick Tagger - ujo.guru 2019
 
 tag_main () { 
 
-	tag_file_name="$1"; shift 					#; echo "|$tag_file_name|"	
-	tag_action="$1"; shift 						#; echo "|$tag_action|"
-	tag_file_format="${tag_file_name: -6}" 		#; echo "|$tag_file_format|" # read six last characters of filename
-	tag_file_format="${tag_file_format#*.}"		#; echo "|$tag_file_format|" # read after separator
-	tag_file_format="${tag_file_format^^}" 		#; echo "|$tag_file_format|" # upcase
+	# get arguments 							# debug
+	tag_file_name="$1"; shift 					#; echo "tag_file_name:$tag_file_name|"	
+	tag_action="$1"; shift 						#; echo "tag_action:$tag_action|"
+	
+	# parse file format
+	tag_file_format="${tag_file_name: -5}" 		#; echo "tag_file_format:$tag_file_format|"		# read last characters of filename
+	tag_file_format="${tag_file_format#*.}"		#; echo "tag_file_format:$tag_file_format|" 	# read after separator
+	tag_file_format="${tag_file_format^^}" 		#; echo "tag_file_format:$tag_file_format|" 	# upcase
 
 	case "$tag_file_format" in 
 
@@ -25,33 +28,66 @@ tag_main () {
 }
 
 
-function tag_audio () {
+tag_audio () {
+	# Audio tagging tools
 
-	echo "audiotag TODO"
-	return 0
+	tag_tool="mid3v2"
+	tag_container="TIT3"
+	
+	ls_tag () { 
+		current_tags=$($tag_tool -l $tag_file_name |grep $tag_container) 
+		current_tags=${current_tags##*=}
+		[[ $current_tags == "" ]] || echo "${current_tags//,/ }"
+		}
+
+	add_tag () { 		
+		current_tags=$(ls_tag)															#; echo "current_tags:$current_tags|"; echo "new tags:$@|"		
+		[[ $current_tags == "" ]] && current_tags="$GURU_USER $GURU_TEAM"
+		$tag_tool --$tag_container "${current_tags// /,},${@// /,}"  "$tag_file_name" 	# use "," as separator to use multible tags
+		}	
+
+	rm_tag () { 	
+		$tag_tool --delete-frames="$tag_container" "$tag_file_name" 								
+		}						
+	
+	case "$tag_action" in
+	
+		rm)
+			rm_tag 
+			;;
+		add)			
+			[[ "$@" ]] && add_tag "$@" 
+			;;
+		ls|"")						
+			ls_tag 
+			;;
+		*)			
+			[[ "$@" ]] && string="$tag_action $@" || string="$tag_action"
+			[[ "$tag_action" ]] && add_tag "$string" 			
+			;;
+		esac
 }
 
 
-function tag_picture () {
+tag_picture () {
+	# Picture tagging tools
 
-	tag_container="Comment"
+	tag_container="Comment" 			# the title under which the information is stored in the image
+	tag_tool="exiftool"
 
 	add_tag () { 
-		_value="$@"		
-		current_tags=$(exiftool -$tag_container $tag_file_name)
+		current_tags=$($tag_tool -$tag_container "$tag_file_name")
 		current_tags=${current_tags##*": "}
 		[[ $current_tags == "" ]] && current_tags="$GURU_USER $GURU_TEAM"
-		exiftool -$tag_container="$current_tags $_value" "$tag_file_name" -overwrite_original_in_place -q 	
-		# current_tags=$(exiftool -$tag_container $tag_file_name); echo "$tag_file_name tags:${current_tags##*:}"
+		$tag_tool -$tag_container="$current_tags $@" "$tag_file_name" -overwrite_original_in_place -q 	
 	}
 
 	rm_tag () { 	
-		exiftool -$tag_container= "$tag_file_name" -overwrite_original_in_place -q 							
-		# #current_tags=$(exiftool -$tag_container $tag_file_name); #echo "$tag_file_name tags:${current_tags##*:}"
+		$tag_tool -$tag_container= "$tag_file_name" -overwrite_original_in_place -q 							
 	}
 
 	ls_tag () { 
-		current_tags=$(exiftool -$tag_container $tag_file_name) 
+		current_tags=$($tag_tool -$tag_container "$tag_file_name") 
 		current_tags=${current_tags##*": "}
 		[[ $current_tags == "" ]] || echo "$current_tags"
 	}
@@ -78,16 +114,21 @@ function tag_picture () {
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
 	
 	case "$1" in 
+
 	install)
-		sudo apt install libimage-exiftool-perl
+		sudo apt install libimage-$tag_tool-perl 		# install picture tag tool
+		sudo apt-get install python-mutagen 			# install mp3 tag tool
 		exit 0
 		;;
+
 	uninstall)
-		sudo apt remove libimage-exiftool-perl
+		sudo apt remove libimage-$tag_tool-perl 		# remove picture tag tool
+		sudo apt-get remove python-mutagen 				# remove mp3 tag tool
 		exit 0
 		;;
+
 	*)	
 		tag_main "$@"
-		;;
 	esac
+
 fi
