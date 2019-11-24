b4232639d5215dd59e0822b0c736630425d71db8
casa <casa@ujo.guru>
2019-11-24 19:42:58 +0200

    feed list from file and point to article

--- src/uutiset.py
+++ src/uutiset.py
@@ -1,25 +1,12 @@
 #!/usr/bin/python3
 # rrs news feed
-# RSS (Rich Site Summary) is a format for delivering regularly changing web content.
 # add to install: sudo pip install --upgrade pip; sudo pip install feedparser
-
-
-
-
-# >>> d.feed.description
-# u'For documentation <em>only</em>'
-# >>> d.feed.published
-# u'Sat, 07 Sep 2002 00:00:01 GMT'
-# >>> d.feed.published_parsed
-# (2002, 9, 7, 0, 0, 1, 5, 250, 0)
 
 import os
 import sys
-#import nltk
 import ast
 import json
-
-
+import readline
 from os import system
 from datetime import datetime
 
@@ -28,7 +15,6 @@ try:
 except ModuleNotFoundError:
 	os.system('pip install --upgrade pip')
 	os.system('sudo -H pip install feedparser')
-	#exit(124)
 finally:
 	import feedparser
 
@@ -37,54 +23,35 @@ try:
 except ModuleNotFoundError:
 	os.system('pip install --upgrade pip')
 	os.system('sudo -H pip install beautifulsoup4')
-	#exit(124)
 finally:
 	from bs4 import BeautifulSoup
-
 
 try:
 	import xml.etree.ElementTree as ET
 except ModuleNotFoundError:
 	os.system('pip install --upgrade pip')
 	os.system('sudo -H pip install elementpath')
-	#exit(124)
 finally:
 	import xml.etree.ElementTree as ET
 
 
 # later to config file [feed_name] url=, date_format= ..
-
 wide = 120
 height = 24
-list_length = height - 3
+list_lenght = 0
+list_lenght_max = height - 3
 
-feed_list = ["https://feeds.yle.fi/uutiset/v1/recent.rss?publisherIds=YLE_UUTISET",\
-			"http://feeds.bbci.co.uk/news/world/rss.xml",\
-			"http://feeds.reuters.com/Reuters/worldNews",\
-			"http://www.cbn.com/cbnnews/world/feed/",\
-			"http://feeds.bbci.co.uk/news/technology/rss.xml",\
-			"https://hackaday.com/blog/feed/"]
-
-feed_list_names = ["yle tuoreimman",\
-					"bbc world",\
-					"reuters world",\
-					"cbn world",\
-					"bbc tech",\
-					"hackaday tech"]
+# Open rrs-feed list file    
+feed_list_file = open (os.environ["GURU_CFG"]+"/rrs-feed.list", "r")
+feed_list = feed_list_file.readlines()
+feed_list_file.close()
 
 # date format https://pythonhosted.org/feedparser/date-parsing.html
-known_format = ['%a, %d %b %Y %H:%M:%S GMT',\
-				'%a, %d %b %Y %H:%M:%S +0300',\
-				'%a, %d %b %Y %H:%M:%S +0000',\
-				'%Y-%m-%dT%H:%M:%S-05:00',\
-				'%Y-%m-%dT%H:%M:%S-04:00',\
-				'%a, %d %b %Y %H:%M:%S -0400',\
-				'%a, %d %b %Y %H:%M:%S -0500',\
-				'%a, %d %b %Y %H:%M:%S-0400'] 			# http://strftime.org/
+known_format = ['%a, %d %b %Y %H:%M:%S',\
+				'%Y-%m-%dT%H:%M:%S'] 			# http://strftime.org/
 
 
 def ulos():
-
 	print("exit")
 	resize_terminal_x11(24, 80)	
 	os.system('clear')
@@ -111,21 +78,26 @@ def print_header ():
 def print_feed ():
 
 	for i in range(len(feed.entries)):
+		global list_lenght
 		entry = feed.entries[i]
 
-		if i > list_length:
+		if i > list_lenght_max:
 			break
 		
 		for format_count in range(len(known_format)):
+			#print(known_format)	
 			try:
-				datestamp = datetime.strptime(entry.published, known_format[format_count])
+				datestamp = datetime.strptime(entry.published.rsplit(' ', 1)[0].rsplit('-', 1)[0], known_format[format_count])
 				break
 			except ValueError:
-				pass			#print("unknown date format: "+entry.published)			
+				#print("unknown date format: "+entry.published.rsplit(' ', 1)[0].rsplit('-', 1)[0])	
+				pass			
 			except:
-				pass			#print("other fuck-up date format"+entry.published)
+				#print("other fuck-up date format"+entry.published.rsplit(' ', 1)[0].rsplit('-', 1)[0])
+				pass			
 
-		datestamp = datetime.strptime(entry.published, known_format[format_count]).strftime('%d.%m.%y %H:%M')	
+		datestamp = datetime.strptime(entry.published.rsplit(' ', 1)[0].rsplit('-', 1)[0], known_format[format_count]).strftime('%d.%m.%y %H:%M')	
+		
 		title = entry.title.replace("&nbsp;", "")	
 		
 
@@ -137,25 +109,29 @@ def print_feed ():
 		
 		print("["+str(i+1)+"]" +" "+ title[0:wide-20] +" "+ datestamp)	
 	
-	if i < list_length:
-			temp = '\n' * ((list_length-i)-1)
+	if i < list_lenght_max:
+			temp = '\n' * ((list_lenght_max-i)-1)
 			print(temp)
+	list_lenght=i
 	return i
-
 
 
 def open_news (feed_index):
 
 	entry = feed.entries[int(feed_index)-1]
-	summary = entry.summary.replace("&nbsp;", "")	
 	title = entry.title.replace("&nbsp;", "")	
 	link = entry.link.replace("&nbsp;", "")			
 
-	#stuff = str(entry.content)[1:-1]
-	#xml = ET.fromstring(stuff)
-	#print(xml+"\n")
-	#content = xml.iter('type')
-	 
+	try:
+		summary = entry.summary.replace("&nbsp;", "")	
+	except AttributeError:		
+		print("no summary data")
+		return 2
+	except:	
+		return 3
+
+	summary = entry.summary.replace("&nbsp;", "")	
+	
 	try:
 		json_string = entry.content
 		content = ''.join(BeautifulSoup(str(json_string), "html.parser").stripped_strings).split("'")[13].replace('\\n',"\n") # bubblecum
@@ -163,17 +139,16 @@ def open_news (feed_index):
 		content = ""
 	except:	
 		content = ""
-		pass
 
 	os.system('clear')
 	print("\n")
 	print_header()
 	print("\n"+title+"\n\n"+summary)
 	print("\n"+content+"\n\n"+link+"\n") 	
-	print('"o" or "2" to open in browser, enter to return: ', end = '')
+	print('"o" to open in browser, enter to return: ', end = '')
 	answer = input()
 	
-	if answer == "o" or answer == "2":
+	if answer == "o":
 		profile = '--user-data-dir='+os.environ["GURU_CHROME_USER_DATA"]
 		browser = os.environ["GURU_BROWSER"]+' '+profile+' '+entry.link+' &'
 		os.system(browser)
@@ -181,42 +156,45 @@ def open_news (feed_index):
 		return 0
 
 
-
 def user_input():
 
+	global feed, feed_selection, list_lenght
 
-	global feed, feed_selection
+	#print('open news: ', end = '')
+	answer = input('open news: ')
 
-	print('open news: ', end = '')
-	answer = input()
-	
-	if answer == "q" or answer == "999": 
+	if answer == "q" or answer == "exit": 
 		ulos()
-
+	
 	if (answer.isdigit()): 				
 
 		selection = int(answer)
 
-		if (selection < 23):			# is feed selection call
-			open_news(answer)	
-			return 0		
-		
-		if selection < 100:
-			return 1
-
-		selection = selection / 100
-
-		if selection > len(feed_list):
-			return 2
-
+		if selection > 0 and selection < 100:			# is feed selection call
+			if selection <= list_lenght:
+				open_news(answer)						
 		else:
-			feed_selection = selection - 1	
-			integer = int(feed_selection) 
-			feed = feedparser.parse(feed_list[integer])	
+			sub_selection = selection % 100			
+			select_int = int(selection / 100 - 1)  		# is float
+			
+			if select_int > len(feed_list) or select_int < 0:
+				return 2			
+			
+			try:
+				feed = feedparser.parse(feed_list[select_int])	
+			except:	
+				return 2
 
+			feed = feedparser.parse(feed_list[select_int])	
+			
+			if sub_selection > 0: 								
+				
+				if (sub_selection) > list_lenght:
+					return 2
+
+				open_news(sub_selection)	
 
 ## Main
-
 resize_terminal_x11(height, wide)
 feed_selection = 0
 feed = feedparser.parse(feed_list[feed_selection])
@@ -224,6 +202,4 @@ feed = feedparser.parse(feed_list[feed_selection])
 while 1:
 	print_header()
 	print_feed()
-	user_input()
-	
-	
+	user_input()	
