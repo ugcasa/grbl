88bbb0662d3eb1fa189a9609d83ffa1a161b796d
casa <casa@ujo.guru>
2020-02-21 00:35:38 +0200

    ssh key add functions up to date

--- src/ssh.sh
+++ src/ssh.sh
@@ -2,52 +2,70 @@
 ## bash script to add SSH key to remote service provider
 # tested: 2/2020 ubuntu desktop 18.04 and mint cinnamon 19.2 
 
-# Issues
-# SC2015: Note that A && B || C is not if-then-else. C may run when A is true.
-# Yes: in this case  this if fine
-
-# SC2143: Use grep -q instead of comparing output with [ -n .. ].
-# SC2086: Double quote to prevent globbing and word splitting.
-# Yes: in this case this if fine: no input that may split
-
 # Inclides
-. lib/common.sh
+#. lib/common.sh
 
-ssh_main(){
+
+ssh_main() {
+    # main selector off ssh functions
+    command="$1"
+    shift
+    echo "$command"
+    case $command in 
+        
+        add-key)
+            ssh_add_key "$@"
+            ;;
+        help|*)
+            printf "\nUsage:\n\t$0 [command] [variables]\n"
+            printf "\nCommands:\n"
+            printf " add-keys   adds keys to server. \n\n"
+            printf "            guru ssh add-keys help for more information.\n\n"
+            ;;
+    esac
+}
+
+
+ssh_add_key(){
     error="1"  # Warning "1" is default exit code
-
+    # [1] ujo.guru, [2] git.ujo.guru, [3] github, [4] bitbucket
     # Install requirements
     xclip -help >/dev/null 2>&1 ||sudo apt install xclip
 
     [ -d "$HOME/.ssh" ] || mkdir "$HOME/.ssh"
 
     # Select git service provider
-    [ "$1" ] && remote="$1" ||read -r -p "[1] github, [2] bitbucket or [3] other: " remote
+    [ "$1" ] && remote="$1" ||read -r -p "[1] ujo.guru, [2] git.ujo.guru, [3] github, [4] bitbucket : " remote
     shift 
+
+    echo "$remote"
+    echo "$1"
 
     case $remote in
         
         1|ujo.guru)
             add_key_accesspoint "$@"
             ;;
-        2|github)
-            add_key_github "$@"
-            error="$?"
-            ;;
-        3|bitbucket)
-            add_key_bitbucket "$@"
-            error="$?"
-            ;;        
-        4|other)
+        2|other|git.ujo.guru)
             my-git_ssh_set "$@"
             error="$?"
             ;;
+        3|github)
+            add_key_github "$@"
+            error="$?"
+            ;;
+        4|bitbucket)
+            add_key_bitbucket "$@"
+            error="$?"
+            ;;        
         help|*)
            printf "Usage: \n\t%s [remote_provider] [login_email]\n" "$0"
-           printf "Remote_provider:\n1|github        github.com account\n"
-           printf "2|bitbucket     bitbucket.org account \n"
-           printf "3|other         other account\n"
-           printf "without variables script asks input during process\n"
+           printf "\nRemote_provider:\n\n"
+           printf "1|ujo.guru      add key to server and rule to .ssh/config \n"
+           printf "2|git.ujo.guru  add key to own git server \n"
+           printf "3|github        add key to github.com \n"
+           printf "4|bitbucket     add key to bitbucket.org \n\n"
+           printf "without variables script asks input during process\n\n"
     esac
 
     if [[ "$error" -gt "1" ]]; then 
@@ -58,13 +76,13 @@ ssh_main(){
     return 0
 }
 
-add_key_accesspoint () {
+add_key_accesspoint () {        # Non tested!!!
     # function to setup ssh key login with github 
     
     local key_file="$HOME/.ssh/ujo.guru_id_rsa"
     local ssh_key_add_url="ujo.guru"
 
-    [ "$1" ] && user_name="$1" || read -r -p "github login email: " user_name
+    [ "$1" ] && user_name="$1" || read -r -p "ujo.guru login name: " user_name
 
     ## Generate keys
     ssh-keygen -t rsa -b 4096 -C "$user_name" -f "$key_file" && echo "Key OK" || return 22
