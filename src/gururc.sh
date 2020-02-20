3de7f2493903a06f44bc964a0ed5c94639ab3311
casa <casa@ujo.guru>
2020-02-20 12:55:57 +0200

    clean up

--- src/gururc.sh
+++ src/gururc.sh
@@ -9,30 +9,21 @@ export GURU_CFG="$HOME/.config/guru"
 export GURU_USER_RC="$GURU_CFG/$GURU_USER/userrc"
 
 if [ -f "$GURU_USER_RC" ]; then 					# if user setting file exist
-
 	. $GURU_USER_RC									# execute personal settings
-
 else 												# Defaults
-
 	export GURU_BIN="$HOME/bin"
 	export GURU_CALL="guru"
 	export GURU_DOMAIN_NAME="ujo.guru"
 
 # userrc
 	export GURU_REAL_NAME="Roger von Gullit"
+	export GURU_USER_EMAIL="$GURU_USER@ujo.guru"
+	export GURU_USER_PHONE="-your phone number-"
+	export GURU_NOTE_HEADER="Notes" 				# TODO koostetaan valmiiksi sringi tässä
 
 # (to teamrc)
-	export GURU_TEAM="guru"
-	export GURU_TEAM_NAME="ujo.guru"
-	export GURU_NOTE_HEADER="Notes" 				# TODO koostetaan valmiiksi sringi tässä
-	export GURU_USER_PHONE="-your phone number-"
-	export GURU_USER_EMAIL="$GURU_USER@ujo.guru"
-
-# Formats (in teamrc)
-	export GURU_DATE_FORMAT='%d.%-m.%Y'
-	export GURU_TIME_FORMAT='%H:%M:%S'
-	export GURU_FILE_DATE_FORMAT='%Y%m%d'
-	export GURU_FILE_TIME_FORMAT='%H%M%S'
+	export GURU_TEAM="test"
+	export GURU_TEAM_NAME="tester-group"
 
 # Folders (in teamrc)
 	export GURU_NOTES="$HOME/Notes"
@@ -47,6 +38,27 @@ else 												# Defaults
 	export GURU_SCAN="$HOME/Documents" 		
 	export GURU_ACCOUNTING="$HOME/Economics"
 	export GURU_PERSONAL_ACCOUNTING="$HOME/Documents"
+
+# Preferred applications
+	export GURU_TERMINAL="gnome-terminal"
+	export GURU_EDITOR="subl"
+	export GURU_BROWSER="firefox"
+	export GURU_OFFICE_DOC="libreoffice"
+	export GURU_OFFICE_SPR="libreoffice"
+
+# Keyboard bindings 
+	export GURU_KEYBIND_TERMINAL="F1"
+	export GURU_KEYBIND_NOTE="<Ctrl>n"
+	export GURU_KEYBIND_DATESTAMP=""
+	export GURU_KEYBIND_TIMESTAMP="<Ctrl>t"
+	export GURU_KEYBIND_SIGNATURE=""
+	export GURU_KEYBIND_PICTURE_MD=""
+
+# Formats (in teamrc)
+	export GURU_DATE_FORMAT='%d.%-m.%Y'
+	export GURU_TIME_FORMAT='%H:%M:%S'
+	export GURU_FILE_DATE_FORMAT='%Y%m%d'
+	export GURU_FILE_TIME_FORMAT='%H%M%S'
 
 # Tool folders (lowercase)
 	export GURU_BIN="$HOME/bin"
@@ -63,21 +75,6 @@ else 												# Defaults
 # Flags
 	export GURU_AUDIO_ENABLED=true
 	export GURU_INSTALL="desktop"
-
-# Preferred applications
-    export GURU_TERMINAL="gnome-terminal"
-	export GURU_EDITOR="subl"
-	export GURU_BROWSER="firefox"
-	export GURU_OFFICE_DOC="libreoffice"
-	export GURU_OFFICE_SPR="libreoffice"
-
-# Keyboard bindings 
-    export GURU_KEYBIND_TERMINAL="F1"
-    export GURU_KEYBIND_NOTE="<Ctrl>n"
-    export GURU_KEYBIND_DATESTAMP=""
-    export GURU_KEYBIND_TIMESTAMP="<Ctrl>t"
-    export GURU_KEYBIND_SIGNATURE=""
-    export GURU_KEYBIND_PICTURE_MD=""
 fi
 
 alias "$GURU_CALL"=$GURU_BIN/guru 					
