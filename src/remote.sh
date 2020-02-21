ada46c40a81da3bac31f44e27cd281a5c18f2c6d
casa <casa@ujo.guru>
2020-02-21 13:31:28 +0200

    pull/push added, active help

--- src/remote.sh
+++ src/remote.sh
@@ -2,6 +2,86 @@
 # sshfs mount functions for guru tool-kit
 # casa@ujo.guru 2020
 
+remote_main() {
+
+    command="$1"
+    shift
+    
+    case "$command" in
+
+        install)
+                install_requirements "$@"
+                ;;
+
+        mount)
+                if [ "$1" == "all" ]; then 
+                    mount_guru_defaults 
+                else
+                    mount_sshfs "$1" "$2"           
+                fi
+                ;;
+
+        unmount|umount)           
+                [ "$1" == "all" ] && unmount_guru_defaults ||mount_sshfs "unmount" "$1"
+                ;;
+
+        ls|list)
+                grep "sshfs" < /etc/mtab
+                ;;
+
+        pull)
+                case $1 in
+                    all )
+                        pull_guru_config_file
+                        #+ other
+                        ;;
+                    
+                    config|cfg)
+                        pull_guru_config_file
+                        ;;
+                        *)
+                        echo "guru remote pull [config|all]"
+                esac            
+            ;;
+
+        push)
+                case $1 in
+                    all )
+                        push_guru_config_file
+                        #+ other
+                        ;;
+                    
+                    config|cfg)
+                        push_guru_config_file
+                        ;;
+                        *)
+                        echo "guru remote push [config|all]"
+                esac            
+            ;;
+
+        help|*)
+                printf "\nUsage:\n\t $0 [command] [arguments]\n\t $0 mount [source] [target]\n"
+                printf "\nCommands:\n\n"
+                printf " ls                     list all active mounts\n" 
+                printf " mount [all]            mount remote filesystem [folder_in_fileserver] [mount_point] \n"
+                printf "                        [all] mount guru defaults \n\t\t\tWarning! Mount point may be generic location as '~/Pictures'  \n"
+                printf " unmount [all]          unmount [mount_point]\n"
+                printf "                        [all] unmount guru defaul locations\n"
+                printf " pull [variable]        [cfg|config] get personal config from server and replace guru \n\t\t\t'./config/guru/%s/userrc' file \n" "$GURU_USER"
+                printf "                        [all] gets all from server (not sure wha all mean for now)\n"
+                printf " push [variable]        [cfg|config] sends current user config to %s \n" "$GURU_ACCESS_POINT_SERVER"
+                printf "                        [all] send all to server (not sure wha all mean for now)\n"
+                printf " install                install requirements\n"   
+                printf "\nExample:\n\t %s remote mount /home/%s/share /home/%s/mount/%s/ \n\n"\
+                       "$GURU_CALL" "$GURU_REMOTE_FILE_SERVER_USER" "$USER" "$GURU_REMOTE_FILE_SERVER"
+    esac
+
+    return 0
+}
+
+install_requirements() {
+    sudo apt install sshfs
+}
 
 mount_sshfs() {
     #mount [what] [where] 
@@ -24,10 +104,14 @@ mount_sshfs() {
 
     # mount
     if [ "$remote_flag" ]; then 
-        sshfs -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3 -p "$GURU_REMOTE_FILE_SERVER_PORT" "$USER@$GURU_REMOTE_FILE_SERVER:$source_folder" "$target_folder"
-    else
-        sshfs -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3 -p "$GURU_LOCAL_FILE_SERVER_PORT" "$USER@$GURU_LOCAL_FILE_SERVER:$source_folder" "$target_folder"
-    fi
+            server="$GURU_REMOTE_FILE_SERVER"             
+            server_port="$GURU_REMOTE_FILE_SERVER_PORT"
+        else
+            server="$GURU_LOCAL_FILE_SERVER"
+            server_port="$GURU_LOCAL_FILE_SERVER_PORT"
+    fi 
+
+    sshfs -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3 -p "$server_port" "$GURU_USER@$server:$source_folder" "$target_folder"
 
     return "$?"
 }
@@ -55,36 +139,23 @@ unmount_guru_defaults() {
 }
 
 
+pull_guru_config_file(){
+    rsync -rvz --quiet -e "ssh -p $GURU_ACCESS_POINT_SERVER_PORT" \
+        "$GURU_USER@$GURU_ACCESS_POINT_SERVER:/home/$GURU_USER/usr/cfg/$GURU_USER.userrc.sh" \
+        "$GURU_CFG/$GURU_USER/userrc" 
+}
+
+
+push_guru_config_file(){
+    rsync -rvz --quiet -e "ssh -p $GURU_ACCESS_POINT_SERVER_PORT" \
+        "$GURU_CFG/$GURU_USER/userrc" \
+        "$GURU_USER@$GURU_ACCESS_POINT_SERVER:/home/$GURU_USER/usr/cfg/$GURU_USER.userrc.sh"
+}
+
+
 # if not runned from terminal, use as library
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-
-    command="$1"
-    shift
-    
-    case "$command" in
-
-        install)
-            sudo apt install sshfs
-            ;;
-        mount|connect)
-            if [ "$1" == "all" ]; then 
-                mount_guru_defaults 
-            else
-                mount_sshfs "$1" "$2"           
-            fi
-            ;;
-
-        unmount|umount|remove)           
-            [ "$1" == "all" ] && unmount_guru_defaults ||mount_sshfs "unmount" "$1"
-            ;;
-
-        ls|list)
-            grep "sshfs" < /etc/mtab
-
-            ;;
-        *)
-        echo "Usage: $0 [unmount|umount|ls] [source_folder_at_server|all] [target_folder_local_fs]"
-    esac
+    remote_main "$@"
     exit 0
 fi
 
