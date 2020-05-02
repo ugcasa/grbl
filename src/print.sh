0468ef34312cce752417af32a8d49e65c1804334
ugcasa <casa@ujo.guru>
2020-05-02 17:30:43 +0300

    label printer tools started

---
+++ src/print.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# sshfs mount functions for guru tool-kit
+
+source $GURU_BIN/lib/common.sh
+
+print.main() {
+    [[ "$GURU_INSTALL" == "server" ]] && print.warning
+
+    command="$1"; shift
+    case "$command" in
+             label|help)    print.$command "$@"                           ; return $? ;;
+                   test)    source $GURU_BIN/test.sh; print.test "$@"     ; return $? ;;
+                      *)    print.help ;;
+    esac
+    return 0
+}
+
+
+print.help () {
+    echo "-- guru tool-kit print help -----------------------------------------------"
+    printf "usage:\t %s print [command] [arguments] \n\t $0 print [source] [target] \n" "$GURU_CALL"
+    printf "\ncommands:\n"
+    printf " label                     print label \n"
+    printf "\nexample:"
+    printf "    %s print mount /home/%s/share /home/%s/mount/%s/\n" "$GURU_CALL" "$GURU_ACCESS_POINT_USER" "$USER" "$GURU_ACCESS_POINT"
+}
+
+
+print.label () {
+    echo "printing label"
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    source "$HOME/.gururc"
+    print.main "$@"
+    exit 0
+fi
+
