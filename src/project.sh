a793bb6c9b57de037d78dfbb1290c1eefd0ee86c
casa <casa@ujo.guru>
2020-05-22 16:24:12 +0300

    project.sh tdd started

--- src/project.sh
+++ src/project.sh
@@ -3,75 +3,55 @@
 # ujo.guru 2020
 
 project.main() {
-
-    command="$1"; shift
+    # command paerser
+    command="$1" ; shift
 
     case "$command" in
-
-        open)
-            project.open "$@"
-            ;;
-
-        test)
-            project.test "$@"
-            ;;
-
-        help)
-            printf "\nguru tool-kit project tools - help \n"
-            printf "\nUsage:\n\t %s [command] <project_name> \n" "$GURU_CALL"
-            printf "\nCommands:\n"
-            printf " open            Open named projec \n"
-            printf "\nExample:\n"
-            printf "\t %s project open guru.ui \n" "$GURU_CALL"
-            echo
-            ;;
-
-        *)
-            project.open "$command"
-            ;;
+        open) project.open $@            ;;
+        test) project.test $@            ;;
+        help) project.help               ;;
+           *) project.open "$command $@" ;;
     esac
+}
 
-    return 0
+
+project.help () {
+    printf "project module works only with sublime.\n"
+    printf "Set preferred editor by typing: %s set editor subl, or edit '~/.gururc'. \n" "$GURU_CALL"
 }
 
 
 project.open () {
-
-    [ "$1" ] && project_name="$1" ||read -p "plase enter project name : " project_name
+    # open project with preferred editor
+    [[ "$1" ]] && _project_name="$1" ||read -p "plase enter project name : " _project_name
     shift
 
-    [ -d "$GURU_PROJECT" ] ||mkdir -p "$GURU_PROJECT"
+    [[ -d "$GURU_PROJECT" ]] ||mkdir -p "$GURU_PROJECT"
 
     case "$GURU_EDITOR" in
-
-            subl|sublime|sublime-text)
-                subl_project_file=$GURU_PROJECT/$project_name.sublime-project
-                if [ -f "$subl_project_file" ]; then
-                    subl --project "$subl_project_file" -a
-                    subl --project "$subl_project_file" -a                              # Sublime how to open workpace?, this works anyway
-                else
-                    printf "no sublime project found" >"$GURU_ERROR_MSG"
-                    return 132
-                fi
-                ;;
-            *)
-                printf "projects work only with sublime. Set preferred editor by typing: %s set editor subl, or edit '~/.gururc'. " "$GURU_CALL" >"$GURU_ERROR_MSG"
-                return 133
+         subl|sublime|sublime-text|subl2|subl3|subl4)
+                                project.sublime $_project_name   ;;
+                            *)  project.help
     esac
-
-    return 0
 }
 
+project.sublime () {
 
-project.test() {
+    local _project_name="$1"                                          ; echo "$_project_name"
+    local _project_file=$GURU_PROJECT/$_project_name.sublime-project  ; echo "$_project_file"
 
-    echo "TEST"
-    project.open "guru-ui" && echo "PASSED" || echo "FAILED"
+    if [ -f "$project_file" ]; then
+        subl --project "$_project_file" -a
+        subl --project "$_project_file" -a                              # Sublime how to open workpace?, this works anyway
+    else
+        printf "no sublime project found" >"$GURU_ERROR_MSG"
+        return 132
+    fi
 }
 
 
 # if not runned from terminal, use as library
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]] ; then
     source "$HOME/.gururc"
     project.main "$@"
     exit 0
