42d309d4f7737ee7eac5b145ac3837b9cd6d18af
casa <casa@ujo.guru>
2020-05-22 18:39:31 +0300

    argument parser added

--- install.sh
+++ install.sh
@@ -9,10 +9,25 @@ GURU_CFG=$HOME/.config/guru
 source src/lib/deco.sh                                          # include decorative functions
 source src/lib/os.sh                                            # include os functions
 source src/keyboard.sh                                          # include keyboard functions
-source src/counter.sh                                           # include keyboard functions
+source src/counter.sh                                           # include counter
 
 target_rc="$HOME/.bashrc"                                       # environmental values rc file
 disabler_flag_file="$HOME/.gururc.disabled"                     # flag for disabling the rc file
+
+
+TEMP=`getopt --long -o "fu:" "$@"`
+eval set -- "$TEMP"
+while true ; do
+    case "$1" in
+        -f ) export force_overwrite=true ; shift ;;
+        -u ) export GURU_USER=$2 ; shift 2 ;;
+         * ) break                  ;;
+    esac
+done;
+_arg="$@"
+[[ "$_arg" != "--" ]] && ARGUMENTS="${_arg#* }"
+
+
 
 case "$1" in                                                    # simple location pased argument parser
     help)
@@ -23,30 +38,31 @@ case "$1" in                                                    # simple locatio
             printf " desktop           desktop install for [ubuntu 18.04>, mint 19.1>19.3]\n"
             printf " server            server install [ubuntu server 18.04>] \n\n"
             ;;
+
 esac
 
 if grep -q ".gururc" "$target_rc" ; then                                                            # already installed? reinstall?
-    [ $force_overwrite ] && answer="y" ||read -p "already installed, force re-install [y/n] : " answer
+    [[ $force_overwrite ]] && answer="y" ||read -p "already installed, force re-install [y/n] : " answer
     if ! [[ "$answer" == "y" ]]; then
         echo "aborting.."
         exit 2
     fi
 
-    [ -f "$GURU_BIN/uninstall.sh" ] && bash "$GURU_BIN/uninstall.sh" || echo "un-installer not found"
+    [[ -f "$GURU_BIN/uninstall.sh" ]] && bash "$GURU_BIN/uninstall.sh" || echo "un-installer not found"
 fi
 
-[ -f "$HOME/.bashrc.giobackup" ] || cp -f "$target_rc" "$HOME/.bashrc.giobackup"                    # Make a backup of original .bashrc but only if installed first time
+[[ -f "$HOME/.bashrc.giobackup" ]] || cp -f "$target_rc" "$HOME/.bashrc.giobackup"                    # Make a backup of original .bashrc but only if installed first time
 grep -q ".gururc" "$target_rc" || cat ./src/tobashrc.sh >>"$target_rc"                              # Check is .gururc called from .bashrc, add call if not
 
-[ -f "$disabler_flag_file" ] && rm -f "$disabler_flag_file"
+[[ -f "$disabler_flag_file" ]] && rm -f "$disabler_flag_file"
 
 cp -f ./src/gururc.sh "$HOME/.gururc"                                                               # create folder structure
 source "$HOME/.gururc"                                                                              # rise default environmental variables
 
-[ -d "$GURU_BIN" ] || mkdir -p "$GURU_BIN"                                                          # make bin folder for script files
-[ -d "$GURU_CFG" ] || mkdir -p "$GURU_CFG"                                                          # make cfg folder for configuration files
-[ -d "$GURU_APP" ] || mkdir -p "$GURU_APP"
-[ -d "$GURU_CFG/$GURU_USER" ] || mkdir -p "$GURU_CFG/$GURU_USER"                                    # personal configurations
+[[ -d "$GURU_BIN" ]] || mkdir -p "$GURU_BIN"                                                          # make bin folder for script files
+[[ -d "$GURU_CFG" ]] || mkdir -p "$GURU_CFG"                                                          # make cfg folder for configuration files
+[[ -d "$GURU_APP" ]] || mkdir -p "$GURU_APP"
+[[ -d "$GURU_CFG/$GURU_USER" ]] || mkdir -p "$GURU_CFG/$GURU_USER"                                    # personal configurations
 cp -f ./cfg/* "$GURU_CFG"                                                                # copy configuration files to configuration folder
 cp -f -r ./src/* -f "$GURU_BIN"                                                                     # copy script files to bin folder
 mv  "$GURU_BIN/guru.sh" "$GURU_BIN/guru"                                                            # rename guru.sh in bin folder to guru
@@ -97,5 +113,9 @@ counter.main add guru-installed >/dev/null                                      
 
 echo "successfully installed"                                                                       # all fine
 exit 0
+
+
+
+
 
 
