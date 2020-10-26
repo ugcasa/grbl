2cdd71a554992976c047f99aaf1ff152e46c7e25
casa <casa@ujo.guru>
2020-10-27 01:37:50 +0200

    re-start to write simple bash based project module (cause i need it roght now)

--- modules/project.sh
+++ modules/project.sh
@@ -2,48 +2,58 @@
 # guru-client project tools
 # ujo.guru 2020
 
+# todo: this bs is. rewrite somehow soon
+
 source $GURU_BIN/common.sh
 source $GURU_BIN/mount.sh
-#source ~/.gururc2
-# echo "TEST: $GURU_SYSTEM_MOUNT"
 
+project.help () {
+    gmsg -v1 -c white "guru-client project help "
+    gmsg -v2
+    gmsg -v0 "usage:    $GURU_CALL project [ls|new|open|status|archive|active|close|rm|sublime|help] <name|id>"
+    gmsg -v2
+    gmsg -v1 -c white "commands:"
+    gmsg -v1 "  ls                      list of archived projects "
+    gmsg -v1 "  new <name|id>           add new projects "
+    gmsg -v1 "  open <name|id>          open project "
+    gmsg -v1 "  close                   close project, keep data "
+    gmsg -v1 "  status <name|id|"">     project status"
+    gmsg -v2 "                          if empty, all project status view"
+    gmsg -v1 "  change <name|id>        same as close and open  "
+    gmsg -v1 "  archive <name|id>       move to archive"
+    gmsg -v2 "                          empty input lists archived projects "
+    gmsg -v1 "  active <name|id>        return archived project "
+    gmsg -v2 "  rm <name|id>            remove project and files for good "
+    gmsg -v2 "  install                 install requirements "
+    gmsg -v2 "  remove                  remove requirements "
+    gmsg -v2 "  sublime <name>          open only sublime project "
+    gmsg -v1 "  help                    this help "
+
+    gmsg -v1
+    gmsg -v1 "most of commands takes project name (or id) as an variable "
+    gmsg -v2
+    gmsg -v1 -c white "example:"
+    gmsg -v1 " $GURU_CALL project new demo       # init new project called 'demo'"
+    gmsg -v2 " $GURU_CALL project archive        # list of archived projects "
+    gmsg -v1 " $GURU_CALL project archive demo   # archive demo "
+    gmsg -v2 " $GURU_CALL project rm demo        # remove all demo project files "
+    gmsg -v2
+
+}
 
 
 project.main() {
     # command paerser
-
     local _cmd="$1" ; shift
-
     case "$_cmd" in
-        add|open|rm|sublime)  project.$_cmd $@           ;;
-                       help)  project.help               ;;
-                     status)  project.status             ;;
-                          *)  project.open "$_cmd"
+        ls|new|status|archive|active|close|rm|help)
+                project.$_cmd $@
+                return $? ;;
+        open)   project.sublime $@
+                return $? ;;
+        *)      project.open "$_cmd"
+                return $? ;;
         esac
-}
-
-
-project.help () {
-    gmsg -v1 -c white "guru-client project module help "
-    gmsg -v2
-    gmsg -v0 "usage:    $GURU_CALL project [add|open|rm|sublime] <project_name>"
-    gmsg -v2
-    gmsg -v1 -c white "commands:"
-    gmsg -v1 "  add             add new projects "
-    gmsg -v1 "  open            open project "
-    gmsg -v1 "  close           close project, keep data "
-    gmsg -v1 "  rm              move project files to trash "
-    gmsg -v1 "  delete          remove project and files for good "
-    gmsg -v1 "  help            this help "
-    gmsg -v1
-    gmsg -v1 "project module works only with sublime."
-    gmsg -v1 "set preferred editor by '%s set editor subl'" "$GURU_CALL"
-    gmsg -v2
-    gmsg -v1 -c white "example:"
-    gmsg -v1 " $GURU_CALL project add projet_name "
-    gmsg -v1 " $GURU_CALL project open project_name "
-    gmsg -v2
-
 }
 
 
@@ -51,74 +61,44 @@ project.check() {
     gmsg -n -v1 "project database.. "
     mount.online "$GURU_SYSTEM_MOUNT"
     if [[ -d "$GURU_SYSTEM_MOUNT/project" ]] ; then
-            gmsg -c green "MOUNTED"
+            gmsg -c green "mounted"
             return 0
         else
-            gmsg -c red "NOT MOUNTED"
+            gmsg -c red "not mounted"
             return 41
         fi
     }
 
 
 project.ls () {
-    gmsg -c $GURU_COLOR_LIST "$(ls $GURU_SYSTEM_MOUNT/project)"
+    gmsg -c light_blue "$(ls $GURU_SYSTEM_MOUNT/project)"
 }
 
 
 project.status () {
     project.check
     project.ls
-    [[ -f $GURU_PROJECT/active ]] && gmsg -c lcyan "currently active $(cat $GURU_PROJECT/active)"  || gmsg -c black "no active projects"
-}
-
-
-project.add () {
-
-    [[ "$1" ]] && _project_name="$1" ||read -p "plase enter project name : " _project_name
-    shift
-
-    if [[ -d "$GURU_PROJECT/$_project_name" ]] ; then
-            EXIST "$_project_name"
-            (( GURU_VERBOSE==2 )) && msg " try another name\n"
-            return 43
-        fi
-
-    mkdir -p $GURU_PROJECT/$_project_name
-}
-
-project.open () {
-    # open project with preferred editor
-    [[ "$1" ]] && _project_name="$1" ||read -p "plase enter project name : " _project_name
-    shift
-
-    if ! [[ -d "$GURU_PROJECT/$_project_name" ]] ; then
-            NOTEXIST "$_project_name"
-            return 43
-        fi
-
-    case "$GURU_EDITOR" in
-         subl|sublime|sublime-text|subl2|subl3|subl4)
-                                project.sublime $_project_name   ;;
-                            *)  project.help
-        esac
+    [[ -f $GURU_PROJECT/active ]] && gmsg -c cyan "currently active $(cat $GURU_PROJECT/active)" || gmsg -c black "no active projects"
 }
 
 
 project.sublime () {
 
-    local _project_name="$1"                                                              ; echo "$_project_name"
-    local _project_file=$GURU_SYSTEM_MOUNT/sublime-projects/$GURU_USER-$_project_name.sublime-project  ; echo "$_project_file"
+    local _project_name="$1"
+    local _project_file=$GURU_SYSTEM_MOUNT/sublime-projects/$GURU_USER-$_project_name.sublime-project
+    # echo "$_project_name"
+    # echo "$_project_file"
 
     if [[ -f "$_project_file" ]] ; then
             subl --project "$_project_file" -a
             subl --project "$_project_file" -a                              # Sublime how to open workpace?, this works anyway
         else
-            WARNING "no sublime project found" #>"$GURU_ERROR_MSG"
+            WARNING "no sublime project found"
             return 132
         fi
 }
 
-# if not runned from terminal, use as library
+
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]] ; then
         source "$GURU_RC"
         project.main "$@"
