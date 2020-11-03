96a4e18c9ccb3d02014c15f99b2cfb7633a27365
casa <casa@ujo.guru>
2020-11-04 01:50:52 +0200

    project ls better

--- modules/project.sh
+++ modules/project.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # guru-client project tools
 # ujo.guru 2020
-
 # todo: this bs is. rewrite somehow soon
-
 source $GURU_BIN/common.sh
 source $GURU_BIN/mount.sh
 
@@ -28,7 +26,6 @@ project.help () {
     gmsg -v2 "  remove                  remove requirements "
     gmsg -v2 "  sublime <name>          open only sublime project "
     gmsg -v1 "  help                    this help "
-
     gmsg -v1
     gmsg -v1 "most of commands takes project name (or id) as an variable "
     gmsg -v2
@@ -38,18 +35,17 @@ project.help () {
     gmsg -v1 " $GURU_CALL project archive demo   # archive demo "
     gmsg -v2 " $GURU_CALL project rm demo        # remove all demo project files "
     gmsg -v2
-
+    return 0
 }
 
 
 project.main() {
     # command paerser
+    project_folder="$GURU_SYSTEM_MOUNT/project"
     local _cmd="$1" ; shift
     case "$_cmd" in
-        ls|new|status|archive|active|close|rm|help)
-                project.$_cmd $@
-                return $? ;;
-        open)   project.sublime $@
+        ls|new|open|status|archive|active|close|rm|sublime|help)
+                project.$_cmd "$@"
                 return $? ;;
         *)      project.open "$_cmd"
                 return $? ;;
@@ -58,27 +54,53 @@ project.main() {
 
 
 project.check() {
-    gmsg -n -v1 "project database.. "
+
+    gmsg -n -v1 "checking project database.. "
     mount.online "$GURU_SYSTEM_MOUNT"
+
     if [[ -d "$GURU_SYSTEM_MOUNT/project" ]] ; then
-            gmsg -c green "mounted"
+            gmsg -c green "ready"
             return 0
         else
             gmsg -c red "not mounted"
-            return 41
+            gmsg -v2 -c white "to mount try '$GURU_CALL mount system'"
+            return 141
         fi
     }
 
 
 project.ls () {
-    gmsg -c light_blue "$(ls $GURU_SYSTEM_MOUNT/project)"
+
+    local _project_list=($(file $project_folder/* | grep directory | cut -d ':' -f1 | rev | cut -d "/" -f1 | rev))
+    gmsg -v3 "project count: ${#_project_list[@]}: ${_project_list[@]}"
+
+    # check active project
+    [[ -f $project_folder/active ]] && local _active_project=$(cat $project_folder/active)
+    gmsg -v3 "activer project: $_active_project"
+
+    # is there projects
+    if (( ${#_project_list[$@]} < 1 )) ; then
+            gmsg -c dark_grey "no projects"
+            return 1
+        fi
+
+    # list of projects
+    gmsg -v1 "list of projects "
+    for _project in ${_project_list[@]} ; do
+        if [[ "$_project" == "$_active_project" ]] ; then
+                gmsg -c cyan " $_project"
+            else
+                gmsg -c light_blue " $_project"
+            fi
+        done
+
+    return 0
 }
 
 
 project.status () {
     project.check
     project.ls
-    [[ -f $GURU_PROJECT/active ]] && gmsg -c cyan "currently active $(cat $GURU_PROJECT/active)" || gmsg -c black "no active projects"
 }
 
 
@@ -86,18 +108,26 @@ project.sublime () {
 
     local _project_name="$1"
     local _project_file=$GURU_SYSTEM_MOUNT/sublime-projects/$GURU_USER-$_project_name.sublime-project
-    # echo "$_project_name"
-    # echo "$_project_file"
+    gmsg -v2 -c dark_grey "opening project $_project_name"
 
     if [[ -f "$_project_file" ]] ; then
+            gmsg -v2 -c dark_grey "using $_project_file"
             subl --project "$_project_file" -a
             subl --project "$_project_file" -a                              # Sublime how to open workpace?, this works anyway
         else
-            WARNING "no sublime project found"
+            gmsg -c yellow "$_project_file not found"
             return 132
         fi
 }
 
+
+project.open () {
+    # just open sublime for now
+    local _project_name=$1
+    gmsg opening
+    echo $_project_name > "$project_folder/active"
+    project.sublime $@
+}
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]] ; then
         source "$GURU_RC"
