03bb9dd989a6865b033b6cbf23e488606772526b
casa <casa@ujo.guru>
2020-11-04 04:11:58 +0200

    terminal size redused

--- modules/audio/voipt.sh
+++ modules/audio/voipt.sh
@@ -59,13 +59,14 @@ voipt.close () {
 
 
 voipt.close_listener () {
-    gnome-terminal -- ssh -p $remote_ssh_port $remote_user@$remote_address $'pkill rx ; pkill socat'
+    gnome-terminal --geometry=36x4 --hide-menubar --zoom=0.5 -- ssh -p $remote_ssh_port $remote_user@$remote_address $'pkill rx ; pkill socat'
     return 0
 }
 
 
 voipt.close_sender () {
-    gnome-terminal -- pkill tx ; pkill socat
+    #gnome-terminal --geometry=36x4 --hide-menubar --zoom=0.5 --
+    pkill tx ; pkill socat
     local pid=$(ps aux | grep ssh | grep "$sender_tcp_port" | tr -s " " | cut -f2 -d " ")
     [[ $pid ]] && kill $pid
     return 0
@@ -75,9 +76,9 @@ voipt.close_sender () {
 voipt.start_listener () {
     # assuming listener is remote and sender is local
     [[ $verbose ]] && echo "listener rx"
-    gnome-terminal -- ssh -p $remote_ssh_port $remote_user@$remote_address $HOME/git/trx/rx -h $sender_address -p $app_udb_port
+    gnome-terminal --geometry=36x4 --hide-menubar --zoom=0.5 -- ssh -p $remote_ssh_port $remote_user@$remote_address $HOME/git/trx/rx -h $sender_address -p $app_udb_port
     [[ $verbose ]] && echo "listener tcp>udp"
-    gnome-terminal -- ssh -p $remote_ssh_port $remote_user@$remote_address socat tcp4-listen:$remote_tcp_port,reuseaddr,fork UDP:$sender_address:$app_udb_port
+    gnome-terminal --geometry=36x4 --hide-menubar --zoom=0.5 -- ssh -p $remote_ssh_port $remote_user@$remote_address socat tcp4-listen:$remote_tcp_port,reuseaddr,fork UDP:$sender_address:$app_udb_port
     return 0
 }
 
@@ -85,12 +86,12 @@ voipt.start_listener () {
 voipt.start_sender () {
     #run listener first**
     [[ $verbose ]] && echo "sender tunnel"
-    gnome-terminal -- ssh -L $sender_tcp_port:$sender_address:$remote_tcp_port $remote_user@$remote_address -p $remote_ssh_port
+    gnome-terminal --geometry=36x4 --hide-menubar --zoom=0.5 -- ssh -L $sender_tcp_port:$sender_address:$remote_tcp_port $remote_user@$remote_address -p $remote_ssh_port -N
     sleep 4
     [[ $verbose ]] && echo "sender tx"
-    gnome-terminal -- /tmp/trx/tx -h $sender_address -p $app_udb_port
+    gnome-terminal --geometry=36x4 --hide-menubar --zoom=0.5 -- /tmp/trx/tx -h $sender_address -p $app_udb_port
     [[ $verbose ]] && echo "sender udp>tcp"
-    gnome-terminal -- socat udp4-listen:$app_udb_port,reuseaddr,fork tcp:$sender_address:$sender_tcp_port
+    gnome-terminal --geometry=36x4 --hide-menubar --zoom=0.5 -- socat udp4-listen:$app_udb_port,reuseaddr,fork tcp:$sender_address:$sender_tcp_port
     return 0
 }
 
