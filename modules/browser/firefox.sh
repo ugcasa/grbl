180a868180d1b01cef7bcb2b6c25a76323bcc976
casa <casa@ujo.guru>
2021-01-08 16:11:18 +0200

    browser module started

---
+++ modules/browser/firefox.sh
@@ -0,0 +1,82 @@
+
+profile_location="$HOME/.config/guru/mozilla"
+
+firefox.add_profile () {
+	local profile_name=$GURU_USER ; [[ $1 ]] && profile_name="$1"
+	local profile="$profile_location/$profile_name"	
+	gmsg -v3 "$profile"
+
+	if ! [[ -d "$profile" ]] ; then 
+			[[ -d "$profile" ]] || mkdir -p "$profile"
+			firefox -CreateProfile "$profile"
+			gmsg -c white "profile added now set your settings, like resize window etc and then exit"
+			firefox --new-instance --profile $profile 
+			# --window-size width=100,height=20   # does not work, informed user to set windows size here
+		else
+			gmsg "profile exist"
+		fi
+}
+
+
+firefox.play_stream () {
+	local profile_name="player"
+	local profile="$profile_location/$profile_name"
+	gmsg -v3 "$profile"
+	firefox --profile $profile --new-window stream.rollfm.fi &
+	# --window-size width=100,height=20   # does not work, but kept in profile
+	
+}
+
+
+firefox.launch () {
+	local profile_name="$GURU_USER"
+	local profile="$profile_location/$profile_name"
+	gmsg -v3 "$profile"
+	firefox --profile $profile  &
+	# --window-size width=100,height=20   # does not work, but kept in profile
+	
+}
+
+firefox.foray () {	
+
+	# DO NOT RUN/CALL THIS FUNCTION
+
+	firefox https://developer.mozilla.org/en-US/docs/Mozilla/Command_Line_Options
+
+	firefox --screenshot $GURU_MOUNT_PICTURES -new-window https://yle.fi/uutiset/tuoreimmat
+	# thiss rolls
+	alias rollfm='firefox --profile rollfm --new-instance --window-size 100,20 stream.rollfm.fi'
+	# this headless shit is
+	firefox --profile rollfm --new-instance stream.rollfm.fi --headless & pid=$! ; read -p "keypress to kill $pid" ; kill -9 $pid
+
+	firefox --profile rollfm --new-instance stream.rollfm.fi --headless & echo $! >/tmp/rollfm.pid
+	kill -9 $(tail /tmp/rollfm.pid) 
+
+	## other noice 
+	# --search <term>   
+	# --private-window 
+	# --kiosk 
+
+	# set focus to adress on browser tab
+	xdotool search --onlyvisible --classname Navigator windowactivate --sync key F6
+
+	# copy adress from browser tab
+	xdotool search --onlyvisible --classname Navigator windowactivate --sync key Ctrl+c
+
+	# get off the focus from adress from browser tab
+	xdotool search --onlyvisible --classname Navigator windowactivate --sync key F6
+
+	# delivery of clipboard content to variable
+	clipboard=`xclip -o -selection clipboard`
+
+	# clear clipboard
+	xsel -bc; xsel -c
+
+	# echo URL of active tab of active browser
+	echo $clipboard
+
+}
+
+source common.sh
+firefox.add_profile player
+firefox.play_stream 
