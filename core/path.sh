6c4281ccdd979fcbfc31a1cece48cdc69996d13b
casa <casa@ujo.guru>
2021-08-30 18:55:48 +0300

    txt -> text again

--- core/path.sh
+++ core/path.sh
@@ -8,7 +8,7 @@ if [[ $GURU_PATH_COLOR_ENABLED ]] && [[ $GURU_FLAG_COLOR ]]; then
 	PS1="$PS1\[$(gmsg -n -C $GURU_PATH_COLOR_HOST)\]@\h"
 	PS1="$PS1\[$(gmsg -n -C $GURU_PATH_COLOR_PROJECT)\]#$GURU_PROJECT_NAME"
 	PS1="$PS1\[$(gmsg -n -C $GURU_PATH_COLOR_FOLDER)\]:\w"
-	export PS1="$PS1\[$(gmsg -n -C $GURU_PATH_COLOR_TXT)\]$ "
+	export PS1="$PS1\[$(gmsg -n -C $GURU_PATH_COLOR_TEXT)\]$ "
 else
 	export PS1='${debian_chroot:+($debian_chroot)}\u@\h#$GURU_PROJECT_NAME:\w$ '
 fi
