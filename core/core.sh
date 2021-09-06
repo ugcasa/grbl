23d205bf29cb992d328ae27d02b72de74a7436c6
casa <casa@ujo.guru>
2021-09-06 23:50:21 +0300

    scripting, probably minor change

--- core/core.sh
+++ core/core.sh
@@ -3,7 +3,7 @@
 # caa@ujo.guru 2020
 
 # global variables variable
-declare -x GURU_VERSION="0.6.6.5"
+declare -x GURU_VERSION="0.6.6.6"
 declare -x GURU_RC="$HOME/.gururc"
 declare -x GURU_BIN="$HOME/bin"
 
