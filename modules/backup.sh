05ae0ce9fad5c0a5c0245e4f1ea7c981771e3563
casa <casa@ujo.guru>
2021-09-28 10:15:35 +0300

    cryptovirus honeypot

--- modules/backup.sh
+++ modules/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# guru-client backup system
-# casa@ujo.guru 2021
+# guru-client backup system casa@ujo.guru 2021
+
 source $GURU_BIN/common.sh
 
 backup.help () {
@@ -34,12 +34,10 @@ backup.help () {
 backup.main () {
     # command parser
 
-    backup_indicator_key="f$(daemon.poll_order backup)"
-
     local command="$1" ; shift
     case "$command" in
 
-                ls|list|now|status|help|install|poll)
+                ls|list|now|status|help|install|poll|all|at)
                     backup.$command  "$@"
                     return $? ;;
 
@@ -63,6 +61,8 @@ backup.config () {
     declare -la backup_name=($1)
     declare -la header=(store method from)
     declare -ga active_list=(${GURU_BACKUP_ACTIVE[@]})
+    declare -g backup_indicator_key="f$(daemon.poll_order backup)"
+
 
     # check is enabled
     if ! [[ $GURU_BACKUP_ENABLED ]] ; then
@@ -110,6 +110,7 @@ backup.config () {
         fi
 
         declare -g storing_method=$method
+        declare -g honeypot_file="$from_location/honeypot.txt"
 
         return 1
 
@@ -121,20 +122,43 @@ backup.config () {
 backup.status () {
     # check latest backup is reachable and returnable.
 
-    GURU_VERBOSE=2
-    backup.config $1
 
+    local backup_indicator_key="f$(daemon.poll_order backup)"
+    local backup_data_folder=$GURU_SYSTEM_MOUNT/backup
 
-    # source $GURU_BIN/os.sh
-    # os.check_space porn
+    gmsg -n -v1 -t "${FUNCNAME[0]}: "
 
-    # if [[ -f  ]] ; then
-    #         gmsg -v1 -c green "ok" -k $backup_indicator_key
-    #         return 0
-    #     else
-    #         gmsg -v1 -c yellow "configuration not found! " -k $backup_indicator_key
-    #         return 1
-    #     fi
+    if [[ $GURU_BACKUP_ENABLED ]] ; then
+            gmsg -n -v1 -c green -k $backup_indicator_key \
+                "enabled, "
+        else
+            gmsg -v1 -c reset -k $backup_indicator_key \
+                "disabled"
+            return 1
+        fi
+
+    if [[ -f $backup_data_folder/next ]] ; then
+            local epic_backup=$(cat $backup_data_folder/next)
+            gmsg -v1 -c aqua -k $backup_indicator_key \
+                "scheduled backup at $(date -d @$epic_backup '+%d.%m.%Y %H:%M')"
+        else
+            gmsg -v1 -c reset -k $backup_indicator_key \
+                "no scheduled backups"
+            return 0
+
+        fi
+
+    if [[ $(date '+%s') -ge $epic_backup ]] ; then
+
+        if backup.all ; then
+            epic_backup=$(( $epic_backup + 86400))
+            echo $epic_backup > $backup_data_folder/next
+            gmsg -c white \
+                "next backup scheduled to $(date -d @$epic_backup '+%d.%m.%Y %H:%M')"
+        fi
+    fi
+
+    return 0
 }
 
 
@@ -144,9 +168,6 @@ backup.list () {
     local ifs=$IFS
     local tall=0
 
-
-
-    # (/home/casa/porn)
     return 0
 }
 
@@ -160,9 +181,28 @@ backup.ls () {
 
 
 backup.at () {
-    # set of cron like launcher based on daemon
+    # set date and time for next backup
 
+    [[ $1 ]] && local backup_date=$1 || read -p "backup date (YYMMDD): " backup_date
+    shift
+    [[ $1 ]] && local backup_time=$1 || read -p "backup time (H:M): " backup_time
+    shift
 
+    local backup_data_folder=$GURU_SYSTEM_MOUNT/backup
+
+    [[ -d $backup_data_folder ]] || mkdir -p $backup_data_folder
+
+    # TBD all kind of checks for user input
+
+    local epic_now=$(date '+%s')
+    local epic_backup=$(date -d "$backup_date $backup_time" '+%s')
+
+    if [[ $epic_now -ge $epic_backup ]] ; then
+        gmsg "that is past, try again"
+        return 1
+    fi
+
+    echo $epic_backup > "$backup_data_folder/next"
 
     # if daemon is not running (computer sleeping) backup is not run
     # overdue backups shall be run at next start
@@ -174,35 +214,96 @@ backup.now () {
     # make backup now
 
     backup.config $1
-
     local from_param="$from_location"
     local store_param="$store_location"
 
+    gmsg -v3 "backup active" -c $GURU_BACKUP_COLOR -k $backup_indicator_key
     #local command_param="-avh '-e ssh -p $from_port' --progress --update"
 
     if [[ $from_domain ]] && [[ $store_domain ]] ; then
             from_param="$from_user@$from_domain 'rsync -ave ssh $from_location $store_user@$store_domain:$from_port:$store_location'"
             store_param=
     elif [[ $from_domain ]] ; then
-            command_param="-avh -e 'ssh -p $from_port' --progress --update"
+            command_param="-a -e 'ssh -p $from_port' --progress --update"
             from_param="$from_user@$from_domain:$from_location"
     elif [[ $store_domain ]] ; then
-            command_param="-avh -e 'ssh -p $store_port' --progress --update"
+            command_param="-a -e 'ssh -p $store_port' --progress --update"
             store_param="$store_user@$store_domain:$store_location"
+        fi
+
+    if ! [[ $store_domain ]] && ! [[ $store_param ]] ; then
+            mkdir -p $store_param
+        fi
+
+    # crypto virus honeypot file
+    # if crypted or copied to another file ending cancel backup
+
+    #echo $honeypot_file
+    if [[ $from_domain ]] ; then
+       gmsg -n "getting honeypot file.. "
+       eval $storing_method $command_param $from_user@$from_domain:$honeypot_file /tmp >/dev/null && gmsg -c green "ok"
     fi
 
-    [[ $store_domain ]]|| echo mkdir -p $store_param
-    echo $storing_method $command_param $from_param $store_param
+    if [[ -f /tmp/honeypot.txt ]] ; then
+            gmsg -n "checking honeypot file.. "
+            local contain=($(cat /tmp/honeypot.txt))
+            if ! [[ ${contain[3]} == "honeypot" ]] ; then
+                gmsg -c yellow \
+                     "honeypot file changed! got '${contain[3]}' when 'honeypot' expected."
+                gmsg -c light_blue "${contain[@]}"
+                gmsg -c red -k $backup_indicator_key \
+                     "backup canceled cause of potential crypto virus action detected!"
+                return 100
 
-    return 0
+                fi
+        gmsg -c green "ok"
+        #rm -f /tmp/honeypot.txt
+        fi
+
+    eval $storing_method $command_param $from_param $store_param
+
+    if [[ $? -gt 0 ]] ; then
+            gmsg -v1 "$from_location error: $storing_method $?" \
+                 -c red -k $backup_indicator_key
+            return 12
+        else
+            gmsg -v3 "$from_location ok" \
+                 -c green -k $backup_indicator_key
+            return 0
+        fi
 }
+
+backup.all () {
+    # backup all in activelist
+
+    local item=1;
+    local error=0
+
+    for source in ${GURU_BACKUP_ACTIVE[@]} ; do
+        gmsg -c dark_golden_rod "backing up $item/${#GURU_BACKUP_ACTIVE[@]}.."
+        backup.now $source || (( _error++ ))
+        (( item++ ))
+        done
+
+
+    if [[ $_error -gt 0 ]] ; then
+        gmsg -v1 "$_error errors, check log above" -c yellow -k $backup_indicator_key
+        return 12
+    else
+        gmsg -v3 "backup done" -c reset -k $backup_indicator_key
+        return 0
+    fi
+
+
+}
+
 
 
 backup.poll () {
     # poll functions
 
     # TBD: write cooperative standard function for timed processes
-
+    local backup_indicator_key="f$(daemon.poll_order backup)"
     local _cmd="$1" ; shift
 
     case $_cmd in
