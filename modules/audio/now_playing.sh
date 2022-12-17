ccc5b8ba46ce0f94bb5c4458260ffeab6bd3e6d9
casa <casa@ujo.guru>
2022-12-18 00:00:53 +0200

    scripting, probably minor change

---
+++ modules/audio/now_playing.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source common.sh
+
+mpv_stat() {
+
+    local _socket='/tmp/mpvsocket'
+
+    mpv_communicate() {
+    # pass the property as the first argument
+      printf '{ "command": ["get_property", "%s"] }\n' "$1" | socat - "${_socket}" | jq -r ".data"
+    }
+
+    [[ -f /tmp/mpvsocket ]] || return 0
+
+    position="$(mpv_communicate "percent-pos" | cut -d'.' -f1)%"
+    file="$(mpv_communicate "filename")"
+    # title="$(mpv_communicate "filename" | sed 's/-/ /g')"
+    playlist_pos="$(mpv_communicate "playlist-pos")"
+    playlist_count="$(mpv_communicate "playlist-count")"
+    volume="$(mpv_communicate "volume")%"
+
+printf "%s %s [%s/%s] %s" "$file" "$position" "$playlist_pos" "$playlist_count" "vol:$volume"
+}
+
+mpv_stat
