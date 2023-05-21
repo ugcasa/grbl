fc85129c9e3902e39c5022a6afd231861e7404a1
casa <casa@ujo.guru>
2023-05-21 18:49:14 +0300

    contrast se added

--- modules/display.sh
+++ modules/display.sh
@@ -2,9 +2,37 @@
 # guru-client display control commands
 # casa@ujo.guru 2022
 
-source common.sh
+#source common.sh
+
 redshift_flag="/tmp/guru-redshift.flag"
 last_brigh="/tmp/guru-display.bright"
+toggle_flag="/tmp/guru-display.toggle.flag"
+
+
+display.help () {
+    # help print depending of verbose level
+    gr.msg -v1 "guru-client display help " -c white
+    gr.msg -v2
+    gr.msg -v0 "usage:    $GURU_CALL display ls|set|reset] "
+    gr.msg -v2
+    gr.msg -v1 -c white  "commands:"
+    gr.msg -v1 " ls                      list of connected monitors"
+    gr.msg -v1 " set <value>             set brightness and contrast to all displays"
+    gr.msg -v2 "                         value between 0.1 .. 2.0"
+    gr.msg -v1 "     contrast <value>    set contrast to all displays"
+    gr.msg -v1 "     brighness <value>   set brightness to all displays"
+    gr.msg -v1 " set br|co               short for brightness and contrast"
+    gr.msg -v1 " reset                   reset values to 1 and re-lauch redshift"
+    gr.msg -v1 " dimm                    set dimmed display settings"
+    gr.msg -v2 " help                    printout this help"
+    gr.msg -v2
+    gr.msg -v1 "example:" -c white
+    gr.msg -v1 "    $GURU_CALL display set 0.75 "
+    gr.msg -v1 "    $GURU_CALL display set brightness 0.8 "
+    gr.msg -v1 "    $GURU_CALL display set co 1.5 "
+    gr.msg -v2
+    gr.msg -v2 "WARNING: values below 0.2 are almost black" -c white
+}
 
 display.main () {
     # command parser
@@ -12,36 +40,58 @@ display.main () {
     shift
 
     case $cmd in
-         reset|set|ls|help|card)
+        reset|set|ls|help|card)
             display.$cmd $@
             return $?
-            ;;
-            *)
-            gr.msg "'$cmd' is not valid command for monitor module"
+        ;;
+
+        toggle)
+            if [[ -f $toggle_flag ]] ; then
+                display.reset
+            else
+                display.set 0.6
+            fi
+        ;;
+
+        dimm)
+            display.set_brightness 0.6
+            display.set_contrast 0.7
+        ;;
+        *)
+            #gr.msg "'$cmd' is not valid command for monitor module"
+            display.reset
             return 1
-            ;;
+        ;;
     esac
 }
 
 
-display.help () {
-    # help print depending of verbose level
-    gr.msg -v1 -c white "guru-client display help "
-    gr.msg -v2
-    gr.msg -v0 "usage:    $GURU_CALL display ls|set|reset] "
-    gr.msg -v2
-    gr.msg -v1 -c white  "commands:"
-    gr.msg -v1 " ls             list of connected monitors"
-    gr.msg -v1 " set <value>    set brightness to all displays"
-    gr.msg -v2 "                value between 0.1 .. 1.0"
-    gr.msg -v1 " reset          set to brightest "
-    gr.msg -v2 "                and restart redshift, if were in use"
-    gr.msg -v2 " help           printout this help"
-    gr.msg -v2
-    gr.msg -v1 -c white  "example:"
-    gr.msg -v1 "    $GURU_CALL display set 0.75 "
-    gr.msg -v2
-    gr.msg -v2 -c white  "WARNING: values below 0.2 are almost black"
+display.set () {
+# set parser
+    local cmd=$1
+    shift
+
+    touch $toggle_flag
+
+    case $cmd in
+        brightness|contrast)
+            display.set_$cmd $@
+        ;;
+
+        br*)
+            display.set_brightness $@
+        ;;
+
+        co*|gamma|ga)
+            display.set_contrast $@
+        ;;
+
+        *)
+            display.set_brightness $cmd
+            display.set_contrast $cmd
+        ;;
+    esac
+    return 0
 }
 
 
@@ -134,8 +184,23 @@ display.ls () {
 }
 
 
-display.set () {
-    # set brightness
+display.set_contrast () {
+# set display contrast (gamma value)
+    local value=1
+    [[ $1 ]] && value=$1
+
+    if [[ $GURU_VERBOSE -gt 1 ]] ; then
+        xgamma -gamma $value
+    else
+        xgamma -gamma $value 2>/dev/null
+    fi
+
+    return $?
+}
+
+
+display.set_brightness () {
+# set brightness
     local value=1
     [[ $1 ]] && value=$1
 
@@ -152,15 +217,16 @@ display.set () {
     #             ;;
     #     esac
 
-    if ps u | grep redshift | grep -v grep ; then
+    while ps auxf | grep redshift | grep -q -v grep ; do
         pkill redshift
-        # indicate user were using redshift
-        [[ -f $redshift_flag ]] || touch $redshift_flag
-    fi
+        sleep 1
+    done
+
+    [[ -f $redshift_flag ]] || touch $redshift_flag
 
     echo "$value" >$last_brigh
 
-    sleep 1
+    sleep 0.5
     # gsettings set org.gnome.settings-daemon.plugins.power ambient-enabled false
 
     display.check
@@ -171,6 +237,7 @@ display.set () {
         xrandr --output ${!stuff} --brightness $value
         sleep 0.5
     done
+
 }
 
 
@@ -185,14 +252,20 @@ display.reset () {
         sleep 0.5
     done
 
+    display.set_contrast 1.0
+
     echo "1" >$last_brigh
 
-    sleep 1
+    while ps auxf | grep redshift | grep -q -v grep ; do
+        pkill redshift
+        sleep 1
+    done
 
-    if [[ $redshift_flag ]] && ! ps u | grep redshift | grep -v grep ; then
+    if [[ -f $redshift_flag ]] ; then
         /usr/bin/redshift >/dev/null 2>/dev/null &
     fi
 
+    [[ -f $toggle_flag ]] && rm $toggle_flag
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
