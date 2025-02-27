9dbdbcf18c1ce8ab6bba58889b6b774e2e45cfe5
Juha Palm <casa@ujo.guru>
2025-02-27 19:42:48 +0200

    script to move guru-cli module updates to grbl branch

---
+++ changes_to_dev.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+source common.sh
+
+_moduleName=$1 
+shift
+
+_moduleType="modules"
+[[ $1 ]] && _moduleType=$1 
+
+[[ $_moduleName ]] || return 0
+
+temp="/tmp/$_moduleName.sh"
+orig=$_moduleType/$_moduleName.sh
+
+[[ -f $temp ]] && rm $temp
+[[ -f $orig ]] || exit 1
+
+cp $orig $temp
+
+sed -i -e 's/guru-client/grbl/g' $temp
+sed -i -e 's/guru-cli/grbl/g' $temp
+sed -i -e 's/guru/grbl/g' $temp
+sed -i -e 's/GURU/GRBL/g' $temp
+sed -i -e 's/ujo.grbl/ujo.guru/g' $temp
+
+git checkout dev || exit 2
+
+if gr.ask "replase $orig with $temp"; then
+	gr.msg "original saved to $temp_orig"
+	cp $orig $temp_original || exit 1
+	gr.ask "OVERWRITE $orig" \
+		&& cp $temp_original $orig \
+		|| gr.msg -e1 "not performed"
+else
+	gr.msg "canceling.."
+fi
+
